a301510c8a05ec28abc9dc67bca3c51a0ee2696e
ffamilyfriendly <jonathan.hermin@skola.sala.se>
2020-12-05 23:49:08 +0100

    added a "test" function

--- console_helper/header.h
+++ console_helper/header.h
@@ -28,6 +28,29 @@ namespace console {
             string colourBG(string input,console::colour bg);
             string colour(string input,console::colour c, console::mode m = console::mode::none);
             string styleReset(string input);
+
+            template <typename T>
+            void runTests(T func(string), string answerToString(T), vector<string> inputs, vector<T> answers) {
+                for(int i = 0; i < inputs.size(); i++) {
+                    T answer = func(inputs[i]);
+                    
+                    string resStr = " PASSED ";
+                    console::colour resCol = console::colour::green;
+
+                    if(answer != answers[i]) {
+                        resStr = " FAILED ";
+                        resCol = console::colour::red;
+                    }
+
+                    if(i > answers.size()-1) {
+                        resStr = " NO ANSWER DEFINED ";
+                        resCol = console::colour::yellow;        
+                    }
+
+                    this->log(this->styleReset( this->colourBG( this->colour(resStr, console::colour::black),resCol)) + " " + inputs[i] + "\nexpected \"" + answerToString(answers[i]) + "\" got \"" + answerToString(answer) + "\"\n");
+                }
+            };
         private:
     };
+
 }
--- console_helper/test.cpp
+++ console_helper/test.cpp
@@ -1,5 +1,17 @@
 #include "header.h"
 
+int strToNumber(string input) {
+    return atoi(input.c_str());
+}
+
+string numberToString(int input) {
+    return to_string(input);
+}
+
+//void runTests(T func, string answerToString , vector<string> inputs, vector<T> answers) {
+
+void runTests(string func(string), string answerToString(string) , vector<string> inputs, vector<string> answers) {}
+
 int main() {
     vector<vector<string>> test;
     console::Console *c = new console::Console();
@@ -10,4 +22,9 @@ int main() {
     
     c->log(c->styleReset( c->colourBG(c->colour("[",console::colour::black),console::colour::red) + c->colour("20",console::colour::yellow,console::mode::bold) + c->colourBG(c->colour("] rows needs checking: ",console::colour::black),console::colour::red) ));
     c->box(test);
+
+    vector<string> inputs = {"0","123","923","this is not a number","this input is wrong","too many inputs"};
+    vector<int> answers = {0,123,923,0,9};
+    
+    c->runTests<int>(strToNumber,numberToString,inputs,answers);
 }
